3b8774d1348b5ba31f6cdbaefb3bf5d6eecd814e
taaenjo1 <joseph.enkaoua@swisscom.com>
2024-08-27 20:47:44 +0200

    ex03 done

---
+++ starting/ex03/capital_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+  states = {
+    "Oregon" : "OR",
+    "Alabama" : "AL",
+    "New Jersey": "NJ",
+    "Colorado" : "CO"
+  }
+  capital_cities = {
+    "OR": "Salem",
+    "AL": "Montgomery",
+    "NJ": "Trenton",
+    "CO": "Denver"
+  }
+  
+  if len(sys.argv) == 2:
+    state = states.get(sys.argv[1])
+    capital = capital_cities.get(state)
+    if capital:
+      print(capital)
+    else:
+      print("Unknown state")
+
+
+if __name__ == '__main__':
+  main()
