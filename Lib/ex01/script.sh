ae9cfee142e7784310650ef45d63ea4ce96acc6e
taaenjo1 <joseph.enkaoua@swisscom.com>
2024-09-09 16:41:17 +0200

    print seperator before executing py file

--- Lib/ex01/script.sh
+++ Lib/ex01/script.sh
@@ -4,6 +4,8 @@ pip3 --version
 python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 install --log pip_install.log -I git+https://github.com/jaraco/path.git
+echo 'Printing installed modules in this environment:'
 pip list
 
+echo '=================== Executing the Python program ==================='
 python3 my_program.py
