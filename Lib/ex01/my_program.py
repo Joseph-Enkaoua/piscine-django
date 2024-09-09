10e6a491a3b2e4a7a3b1943b566abf8a8e2c5a6c
taaenjo1 <joseph.enkaoua@swisscom.com>
2024-09-10 00:22:16 +0200

    02 done

--- Lib/ex01/my_program.py
+++ Lib/ex01/my_program.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 
-from path import Path
+from path import Path # type: ignore
 
 
 def main():
---
+++ Lib/ex02/request_wikipedia.py
@@ -0,0 +1,57 @@
+#!usr/bin/python3
+ 
+import requests, json, dewiki, sys # type: ignore
+
+
+def get_page_content() -> str:
+  S = requests.session()
+  URL = "https://en.wikipedia.org/w/api.php"
+
+  PARAMS = {
+    "action": "opensearch",
+    "namespace": "0",
+    "search": sys.argv[1],
+    "limit": "1",
+    "format": "json"
+  }
+
+  R = S.get(url=URL, params=PARAMS)
+  DATA = R.json()
+
+  if len(DATA[1]) == 0:
+    print("No results found for the search term.")
+    sys.exit(1)
+
+  first_result_title = DATA[1][0]
+
+  PARAMS_CONTENT = {
+    "action": "parse",
+    "format": "json",
+    "prop": "wikitext",
+    "page": first_result_title,
+    "redirects": "true"
+  }
+
+  R2 = S.get(url=URL, params=PARAMS_CONTENT)
+  content_dict = json.loads(R2.text)
+  return dewiki.from_string(content_dict["parse"]["wikitext"]["*"])
+
+
+def main():
+  if len(sys.argv) != 2:
+    print('Please add your search request in one string.\nExample: python3 request_wikipedia.py "airplane"')
+    sys.exit(1)
+
+  try:
+    content = get_page_content()
+  except Exception as e:
+    print(e)
+    sys.exit(1)
+
+  file_name = sys.argv[1].replace(' ', '_')
+  with open(file_name+'.wiki', 'w+') as f:
+    f.write(content)
+
+
+if __name__ == "__main__":
+  main()
