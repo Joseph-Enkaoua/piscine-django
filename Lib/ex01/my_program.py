a99745d715e7a09fc5156d0be4240b7c4061e991
taaenjo1 <joseph.enkaoua@swisscom.com>
2024-09-09 17:11:49 +0200

    01 done

---
+++ Lib/ex01/my_program.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+
+from path import Path
+
+
+def main():
+  try:
+    Path('new-folder').mkdir_p()
+    Path('new-folder/new-file').touch()
+  except Exception as e:
+    print(e)
+
+  with open('./new-folder/new-file', 'w+') as f:
+    f.write('Successfuly done exercise 01')
+    f.seek(0)
+    print('Printing the content of \'new-folder/new-file\':\n')
+    print(f.read())
+
+
+if __name__ == "__main__":
+  main()
--- Lib/ex01/script.sh
+++ Lib/ex01/script.sh
@@ -4,8 +4,8 @@ pip3 --version
 python3 -m venv local_lib
 source local_lib/bin/activate
 pip3 install --log pip_install.log -I git+https://github.com/jaraco/path.git
-echo 'Printing installed modules in this environment:'
+echo -e '\nPrinting installed modules in this environment:\n'
 pip list
 
-echo '=================== Executing the Python program ==================='
+echo -e '\n=================== Executing the Python program ===================\n'
 python3 my_program.py
