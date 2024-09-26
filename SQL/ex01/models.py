3319a69c91418ee5c52b2b6713c196c87216b8af
taaenjo1 <joseph.enkaoua@swisscom.com>
2024-09-26 12:37:59 +0200

    fixed html spacing and model types

--- SQL/ex01/models.py
+++ SQL/ex01/models.py
@@ -7,7 +7,7 @@ class Movies(models.Model):
 
   title = models.CharField(max_length=64, unique=True, null=False)
   episode_nb = models.IntegerField(primary_key=True)
-  opening_crawl = models.TextField(null=True)
+  opening_crawl = models.CharField(max_length=255, blank=True, null=True)
   director = models.CharField(null=False, max_length=32)
   producer = models.CharField(null=False, max_length=128)
   release_date = models.DateField(null=False)
