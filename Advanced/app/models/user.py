c3fbba8b28e1c3fd63b0cf141648e2ad2ca28ec5
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2024-11-25 00:53:32 +0100

    populate users and articales, init login

--- Advanced/app/models/user.py
+++ Advanced/app/models/user.py
@@ -1,8 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+  def create_user(self, username, password=None, **extra_fields):
+    if not username:
+        raise ValueError("The Username field must be set")
+    user = self.model(username=username, **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_superuser(self, username, password=None, **extra_fields):
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+
+    if extra_fields.get('is_staff') is not True:
+        raise ValueError("Superuser must have is_staff=True.")
+    if extra_fields.get('is_superuser') is not True:
+        raise ValueError("Superuser must have is_superuser=True.")
+
+    return self.create_user(username, password, **extra_fields)
+
+  def get_by_natural_key(self, username):
+    return self.get(username=username)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
   username = models.CharField(max_length=150, unique=True)
+  is_staff = models.BooleanField(default=False)  # Required for admin access
+  is_active = models.BooleanField(default=True)  # Required for login
+
+  # Attach the custom manager
+  objects = CustomUserManager()
 
   groups = models.ManyToManyField(
     Group,
