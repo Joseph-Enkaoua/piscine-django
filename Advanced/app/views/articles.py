f8e168b9eaa652a50d4c12df1555ff44a95eb034
Yossi <96576365+Joseph-Enkaoua@users.noreply.github.com>
2024-11-25 02:47:02 +0100

    show created articles in home page

--- Advanced/app/views/articles.py
+++ Advanced/app/views/articles.py
@@ -2,5 +2,6 @@ from django.views.generic import ListView
 from app.models import *
 
 class ArticlesListView(ListView):
-    template_name = 'articles.html'
+    template_name = "articles.html"
     model = Article
+    queryset = Article.objects.filter().order_by('-created')
